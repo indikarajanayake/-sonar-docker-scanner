0e1d121a06f2da7818503e1106c74a1b0dc6229b
Indika <indikarajanayake@gmail.com>
2020-04-13 21:08:33 +1000

    Add aws cli

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,9 @@ From openjdk:8-jre-alpine
 
 RUN apk update && \
     apk upgrade && \
-    apk add --no-cache nodejs npm unzip wget
+    apk add --no-cache nodejs npm unzip wget python py-pip
+
+RUN pip install awscli
 
 RUN wget -O sonar-scanner-cli.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-3.3.0.1492.zip && \
     unzip sonar-scanner-cli.zip && \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sonar-scanner -Dsonar.projectKey=${INPUT_PROJECTKEY} -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=b14e9e2e05a14607b993675dd96a465ea8697b21 -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} -Dsonar.organization=${INPUT_ORGANISATION} -Dsonar.scm.disabled=true
+
+token=$(aws ssm get-parameter --name "sonar_cloud_token" --region us-east-1 --output text --query Parameter.Value)
+sonar-scanner -Dsonar.projectKey=${INPUT_PROJECTKEY} -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$token -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} -Dsonar.organization=${INPUT_ORGANISATION} -Dsonar.scm.disabled=true
